b37901c3d92bb70dc26dc4759ef32c401f78d56d
LuizHenriqueGomesRibeiro <louisnuuk@hotmail.com>
2023-07-16 12:11:52 -0300

    comitar erro

--- JSP/src/main/java/servlets/ServletUsuarioController.java
+++ JSP/src/main/java/servlets/ServletUsuarioController.java
@@ -151,9 +151,15 @@ public class ServletUsuarioController extends ServletGenericUtil {
 
 			if (ServletFileUpload.isMultipartContent(request)) {
 
-				Part part = request.getPart("fileFoto"); 
-				byte[] foto = IOUtils.toByteArray(part.getInputStream());
-				String imagemBase64 = "data:image/"+ part.getContentType().split("\\/")[0]+";base64,"+ new Base64().encodeBase64String(foto);
+				Part part = request.getPart("fileFoto"); /* Pega foto da tela */
+
+				if (part.getSize() > 0) {
+					byte[] foto = IOUtils.toByteArray(part.getInputStream());
+					String imagemBase64 = "data:image/" + part.getContentType().split("\\/")[1] + ";base64," + new Base64().encodeBase64String(foto);
+
+					modelLogin.setFotouser(imagemBase64);
+					modelLogin.setExtensaofotouser(part.getContentType().split("\\/")[1]);
+				}
 
 			}
 
